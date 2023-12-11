2c35c223e4c82820c1ea4fc9e10e1ac1b3d4f1e9
Abhinandan Rauniyar <Abhinandan Rauniyar>
2023-12-11 22:32:50 +0530

    password generator

--- second/src/App.jsx
+++ second/src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
+import { useState,useCallback,useEffect,useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
+
 function App() {
-  let [counter,setCounter]=useState(2);
-  let addValue=()=>{
-    if(counter>=20){
-      alert("Value is more than or equal to 20");
-    } else{
-      setCounter(counter+1);
+  let [length,setLength]=useState(5);
+  let [char,setChar]=useState(false);
+  let [number,setNumber]=useState(false);
+  let [password,setPassword]=useState("");
+  const passwordGenerator=useCallback(()=>{
+    let pass="";
+    let string="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(number) string+="0123456789";
+    if(char) string+="{@#$!%^&*(]";
+    for(let r=1;r<=length;r++){
+      let char=Math.floor(Math.random()*string.length+1);
+      pass+=string.charAt(char);
     }
-   
-  }
-  let subtractValue=()=>{
-    if(counter<=0){
-      alert("Value is less than or equal to 0");
-    } else{
-      setCounter(counter-1);
-    }
-  }
+    setPassword(pass);
+  },[length,char,number,setPassword])
+  useEffect(()=>passwordGenerator(),[length,char,number,setPassword])
+ let passwordRef=useRef(null);
+   let passwordCopyToClipboard=()=>{
+       passwordRef.current?.select();
+       passwordRef.current?.setSelectionRange(0,5);
+    window.navigator.clipboard.writeText(password);
+   }
   return (
     <>
-     <h1>Chai aur react</h1>
-     <h1>Counter:{counter}</h1>
-     <button onClick={addValue}>Add Value</button>
-     <button onClick={subtractValue}>Subtract Value</button>
-    
+      <div style={{background:"red",borderRadius:"10px"}}>
+    <h1 style={{color:"white"}}>Password Generator</h1>
+    <div>
+      <input type="text" 
+      value={password}
+      placeholder='password'
+      readOnly
+      ref={passwordRef}
+      />
+      <button onClick={passwordCopyToClipboard}>Copy</button>
+
+    </div>
+    <div>
+      <input type="range" min={5} max={30} value={length} onChange={(e)=>{setLength(e.target.value)}}/>
+      <label>length:{length}</label>
+      <input type="checkbox" defaultChecked={number} onChange={()=>setNumber((prev)=>!prev)}/>
+      <label>Number Allowed</label>
+      <input type="checkbox" defaultChecked={number} onChange={()=>setChar((prev)=>!prev)}/>
+      <label>Character Allowed</label>
+    </div>
+        
+      </div>
     </>
      
   )
